a27b98ff9dae8b79c09fd257f75152f7e8bcf502
Roy Melhem <roysgap@hotmail.com>
2015-11-03 13:13:50 +0200

    mvc progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comment
+  has_many :comment, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :post, :comment
+  has_many :post
+  has_many :comment
 end
