ae57ee9d2fdcf924839df74f6cd688e1ffdb0e0d
Roy Melhem <roysgap@hotmail.com>
2015-11-03 17:22:55 +0200

    Posts and comments relations logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.new(post_params)
+    @post.user = @user
+
+    if @post.save
+      redirect_to user_path(@user)
+    else
+      render 'users/show'
+    end
+
+    if @post.save
+      respond_to do |format|
+        format.html { redirect_to user_path(@user) }
+        format.js  # <-- will render `app/views/posts/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'users/show' }
+        format.js  # <-- idem
+      end
+    end
+  end
 
   def index
     @posts = Post.all
@@ -6,7 +29,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comment = Comment.new  # <-- You need this now.
+    @comment = Comment.new
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :posts do
-    resources :comments, only: :create
+    resources :comments, only: [ :create, :destroy ]
   end
 
 
