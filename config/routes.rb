a62b473b0101fa2f1819436a92db9395a7c37bad
Roy Melhem <roysgap@hotmail.com>
2015-11-03 15:32:16 +0200

    Post Index & Show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts
+
   root to: 'pages#home'
+  get 'user' => 'pages#user_profile'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
