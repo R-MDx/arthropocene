02c421822152dd9eafdc697675d8a90f52637578
Roy Melhem <roysgap@hotmail.com>
2015-11-09 11:42:11 +0200

    after sign up redirect not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def after_inactive_sign_up_path_for(resource)
+    profile_path
+  end
+
   def after_sign_in_path_for(user)
     public_posts_path
+  end
+
+  def after_sign_out_path_for(user)
+    root_path
   end
 end
