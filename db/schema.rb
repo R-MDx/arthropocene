7517effd405860471e8d9b2ed617232496f5ab67
Roy Melhem <roysgap@hotmail.com>
2015-11-11 22:44:05 +0200

    home page background transparent buttons with jquery under development

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,6 @@ ActiveRecord::Schema.define(version: 20151111095340) do
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "age"
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
@@ -59,6 +56,9 @@ ActiveRecord::Schema.define(version: 20151111095340) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false, null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
