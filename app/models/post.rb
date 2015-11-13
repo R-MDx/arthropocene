9352471741a4add72a639e81ddb07acf67628b39
Roy Melhem <roysgap@hotmail.com>
2015-11-14 01:06:26 +0200

    refacto views

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,13 @@ class Post < ActiveRecord::Base
   validates :user, presence: true
 
   has_many :comments, dependent: :destroy
-  validates :title, :description, :story, presence: true
 
   has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
+
+  validates :title, :description, :story, :picture, presence: true
 end
 
