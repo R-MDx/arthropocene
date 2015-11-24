c83a68d5be2e61de616eac751179d565dc0bb47a
Roy Melhem <roysgap@hotmail.com>
2015-11-24 19:12:55 +0200

    remove auto generated posts from faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,19 +14,19 @@ User.create(
   password: 'foobarbar'
 )
 
-5.times do
-  user = User.new(
-    email: Faker::Internet.email ,
-    password: Faker::Internet.password(8)
-  )
-  user.save
-  2.times do
-    Post.create(
-      title: Faker::Lorem.sentence(1),
-      description: Faker::Lorem.sentence(2),
-      story: Faker::Lorem.paragraph(2, true),
-      user_id: user.id,
-      picture: "https://robohash.org/sedanimifacere.png?size=300x300&set=set1"
-    )
-  end
-end
+# 5.times do
+#   user = User.new(
+#     email: Faker::Internet.email ,
+#     password: Faker::Internet.password(8)
+#   )
+#   user.save
+#   2.times do
+#     Post.create(
+#       title: Faker::Lorem.sentence(1),
+#       description: Faker::Lorem.sentence(2),
+#       story: Faker::Lorem.paragraph(2, true),
+#       user_id: user.id,
+#       picture: "https://robohash.org/sedanimifacere.png?size=300x300&set=set1"
+#     )
+#   end
+# end
